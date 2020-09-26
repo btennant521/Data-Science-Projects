5203ea5701b92425136fdc6b6de7d9a52c1f96b0
Ben Tennant <ben.tennant@du.edu>
2020-09-26 00:17:33 -0600

    data science repo update

---
+++ RNN/airlineClassifier/vectorizer.py
@@ -0,0 +1,29 @@
+from sklearn.feature_extraction.text import HashingVectorizer
+import re
+import os
+import pickle
+from nltk.stem.porter import PorterStemmer
+
+cur_dir = os.path.dirname(__file__)
+stop = pickle.load(open(
+                os.path.join(cur_dir, 
+                'pkl_objects', 
+                'stopwords.pkl'), 'rb'))
+				
+porter = PorterStemmer()
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                           text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) \
+                   + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+vect = HashingVectorizer(decode_error='ignore',
+                         n_features=2**21,
+                         preprocessor=None,
+                         tokenizer=tokenizer_porter)
